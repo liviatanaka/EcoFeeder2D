08a1f1bb838b3655e21cc75699dafaf637273db4
liviatanaka <liviatanaka19@gmail.com>
2024-04-15 12:26:50 -0300

    personagem andando com animação

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+
+    private PlayerControls playerControls;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRender;
+
+
+    private void Awake() {
+        playerControls = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRender = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable() {
+        playerControls.Enable();
+    }
+
+    private void Update() {
+        PlayerInput();
+    }
+
+    private void FixedUpdate() {
+        AdjustPlayerFacingDirection();
+        Move();
+    }
+
+    private void PlayerInput() {
+        // realiza a leitura do input do usuário
+        movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
+        // animação do personagem vertical e horizontal
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
+    }
+
+    private void Move() {
+        // atualiza a posição do usuário
+        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void AdjustPlayerFacingDirection() {
+        // pega a posição do mouse
+        Vector3 mousePos = Input.mousePosition;
+
+        // transforma a posição do espaço do mundo para o espaço da tela
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        // se a posição do mouse for menor que a posição do player, 
+        // ele vira para a esquerda
+        if (mousePos.x < playerScreenPoint.x) {
+            mySpriteRender.flipX = true;
+        } else {
+            mySpriteRender.flipX = false;
+        }
+    }
+}
